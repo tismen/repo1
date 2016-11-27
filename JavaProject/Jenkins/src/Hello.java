75ac6c482af27bba849af2872bfbcf0e644f1ee2
tismen <pbsimon@gmail.com>
2016-11-26 21:41:16 -0500

    Added project to repository

---
+++ JavaProject/Jenkins/src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//modified options
+		System.out.println("Hello World");
+
+	}
+
+}
